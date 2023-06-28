e36cc8a05fb063ff740eac673c3b10971e675c22
Shweta Pal <shwetapal975@gmail.com>
2023-06-28 16:11:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-//your JS code here. If required.
+let submitBtn = document.querySelector('#submit');
+let checkBox = document.querySelector('#checkbox');
+let username = document.querySelector('#username');
+let password= document.querySelector('#password');
+
+submitBtn.addEventListener('click',(event)=>{
+	event.preventDefault();
+// if(username.value === '' || password.value === ''){}
+  if(checkbox.checked){
+  let users =[];
+  let user ={
+   UserName : username.value,
+   Password : password.value}
+   users.push(user);
+   localStorage.setItem('users', JSON.stringify(users));
+  }
+  else if(JSON.parse(localStorage.getItem('users'))){
+  	let savedUser =	JSON.parse(localStorage.getItem('users'));
+    savedUser.forEach((user)=>{
+      if(user.UserName === username.value ){
+      let loginBtn=document.createElement('Button');
+      loginBtn.id= "existing";
+      loginBtn.innerText = `Login as existing user ` ;
+      document.body.append(loginBtn);
+      // let existingUser= document.getElementById('existing');
+      loginBtn.addEventListener('click',(e)=>{
+  		  e.preventDefault();
+  		  alert(`Logged in as ${user.UserName}`)})
+      }
+    });
+  }
+  else{
+		alert(`Logged in as ${username.value}`);
+    }
+});
